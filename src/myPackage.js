d985bc0e2a88c29ef81e0124566906c2965c1bc2
baedollee <baesee0401@gmail.com>
2022-12-17 11:41:45 +0900

    12/17

---
+++ src/myPackage.js
@@ -0,0 +1,15 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {*} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns true;
+ */
+
+export function init(config) {
+  return true;
+}
+export function exit(code) {
+  return code + 1;
+}
